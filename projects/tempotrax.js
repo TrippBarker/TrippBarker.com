6eeabe5797ec26aef3bed88b4fb60f8a91bff02d
Tripp Barker <colemanbarker@gmail.com>
2023-06-23 19:58:20 -0400

    working on authorization

--- projects/tempotrax.js
+++ projects/tempotrax.js
@@ -11,35 +11,3 @@ let body = new URLSearchParams({
   code_verifier: codeVerifier
 });
 
-const response = fetch('https://accounts.spotify.com/api/token', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: body
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('HTTP status ' + response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    localStorage.setItem('access_token', data.access_token);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-
-  async function getProfile(accessToken) {
-    let accessToken = localStorage.getItem('access_token');
-  
-    const response = await fetch('https://api.spotify.com/v1/me', {
-      headers: {
-        Authorization: 'Bearer ' + accessToken
-      }
-    });
-  
-    const data = await response.json();
-  }
-  
