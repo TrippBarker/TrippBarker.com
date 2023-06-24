0f5d29da7a4f600a8b87cf532e9e1cc097cbe14f
Tripp Barker <colemanbarker@gmail.com>
2023-06-23 22:06:09 -0400

    working on authorization

---
+++ projects/tempotraxinit.js
@@ -0,0 +1,47 @@
+function generateRandomString(length) {
+    let str = '';
+    let validChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      str += validChars.charAt(Math.floor(Math.random() * validChars.length));
+    }
+    return str;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    function base64encode(string) {
+      return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+  
+    const encoder = new TextEncoder();
+    const data = encoder.encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+  
+    return base64encode(digest);
+}
+
+const clientId = '8e81373db75b4fc1ab89d7e246c17c73';
+const redirectUri = 'https://www.trippbarker.com/projects/tempotrax';
+
+let codeVerifier = generateRandomString(128);
+
+generateCodeChallenge(codeVerifier).then(codeChallenge => {
+  let state = generateRandomString(16);
+  let scope = 'user-read-private user-read-email';
+
+  localStorage.setItem('code_verifier', codeVerifier);
+
+  let args = new URLSearchParams({
+    response_type: 'code',
+    client_id: clientId,
+    scope: scope,
+    redirect_uri: redirectUri,
+    state: state,
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge
+  });
+
+  window.location = 'https://accounts.spotify.com/authorize?' + args;
+});
