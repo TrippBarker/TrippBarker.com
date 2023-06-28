819211909c351d43e63cfb6e3a487bd877b3146b
Tripp Barker <colemanbarker@gmail.com>
2023-06-28 13:30:41 -0400

    getting profile data

--- projects/tempotrax.js
+++ projects/tempotrax.js
@@ -43,4 +43,20 @@ async function getProfile(accessToken) {
   console.log(data);
 }
 
-getProfile();
+function populateUI(profile) {
+  document.getElementById("displayName").innerText = profile.display_name;
+  if (profile.images[0]) {
+      const profileImage = new Image(200, 200);
+      profileImage.src = profile.images[0].url;
+      document.getElementById("avatar").appendChild(profileImage);
+      document.getElementById("imgUrl").innerText = profile.images[0].url;
+  }
+  document.getElementById("id").innerText = profile.id;
+  document.getElementById("email").innerText = profile.email;
+  document.getElementById("uri").innerText = profile.uri;
+  document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+  document.getElementById("url").innerText = profile.href;
+  document.getElementById("url").setAttribute("href", profile.href);
+}
+
+populateUI(getProfile());
