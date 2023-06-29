963b9e13729b510195953885318506240a205f12
Tripp Barker <colemanbarker@gmail.com>
2023-06-28 22:15:25 -0400

    creating playlist

--- projects/tempotrax.js
+++ projects/tempotrax.js
@@ -70,8 +70,8 @@ async function createPlaylist(){
   accessToken = localStorage.getItem('access_token');
   const response = await fetch('https://api.spotify.com/v1/users/'+localStorage.getItem('userID')+'/playlists',{
     headers: {
-      Authorization: 'Bearer ' + accessToken,
-      'ContentType': 'application/json'
+      'ContentType': 'application/json',
+      Authorization: 'Bearer ' + accessToken
     },
     data: {
       'Name': 'TempoTrax Playlist',
