0647479074ae7a748f15f6b90337f5ea0115a3da
Tripp Barker <colemanbarker@gmail.com>
2023-06-28 20:16:55 -0400

    getting playlists

--- projects/tempotrax.js
+++ projects/tempotrax.js
@@ -69,7 +69,7 @@ async function getPlaylists(){
 
 async function createPlaylist(){
   accessToken = localStorage.getItem('access_token');
-  const response = await fetch('https://api.spotify.com/v1/users/'+localStorage.getItem('userID')+'/playlists',{
+  const response = await fetch('https://api.spotify.com/v1/users/'+localStorage.getItem('userID')+'/playlists', {
     headers: {
       AccessControlAllowOrigin: '*',
       AccessControlAllowMethods: 'GET,HEAD,OPTIONS,POST,PUT',
